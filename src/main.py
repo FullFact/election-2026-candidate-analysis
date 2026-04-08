2daf5084c510471b7f389d5b76f2b92c921b2d4d
nmenezes0 <nemenezes@hotmail.com>
2026-04-08 22:12:20 +0100

    feat: Add more fields for each candidate and more refactoring

--- src/main.py
+++ src/main.py
@@ -1,19 +1,26 @@
 import json
+from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
 
 
 def get_list_of_candidate_data(df: pd.DataFrame) -> list[dict]:
-    allcandidates = []
+    candidates_data_list = []
     for _, row in df.iterrows():
-        dictionary = {
-            "person_name": row["person_name"],
-            "person_id": row["person_id"],
-            "party_name": row["party_name"],
-        }
-        allcandidates.append(dictionary)
-    return allcandidates
+        candidate_dictionary = {}
+        # TODO - remove some of these
+        fields_to_include = [
+            'person_id', 'person_name', 'election_id', 'ballot_paper_id',
+            'election_date', 'election_current', 'party_name', 'party_id',
+            'post_label', 'cancelled_poll', 'seats_contested', 'facebook_page_url',
+            'linkedin_url', 'twitter_username', 'youtube_profile', 'instagram_url',
+            'blue_sky_url', 'threads_url', 'tiktok_url'
+        ]
+        for fieldname in fields_to_include:
+            candidate_dictionary[fieldname] = row[fieldname]
+        candidates_data_list.append(candidate_dictionary)
+    return candidates_data_list
 
 
 def get_candidate_data_by_election_id(df: pd.DataFrame) -> dict:
@@ -25,12 +32,17 @@ def get_candidate_data_by_election_id(df: pd.DataFrame) -> dict:
     return output_dictionary
 
 
+def write_out_json(data: dict | list, filename_suffix: str) -> None:
+    src_dir = Path(__file__).parent
+    data_output_dir = src_dir / "data_outputs"
+    timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    output_path = data_output_dir / f"{timestamp}_{filename_suffix}.json"
+    with open(output_path, "w+") as f:
+        json.dump(data, f)
+
+
 if __name__ == "__main__":
     src_dir = Path(__file__).parent
     df = pd.read_csv(src_dir / "raw_data/dc-candidates-scotland-2026-04-02T16-17-27.csv")
-
     result = get_candidate_data_by_election_id(df)
-    print(json.dumps(result, indent=4))
-
-    with open(src_dir / "data_outputs/candidates.json", "w+") as f:
-        json.dump(result, f)
+    write_out_json(data=result, filename_suffix="candidates_by_election")
